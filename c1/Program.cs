54feb954fd51d42895a1c3a7a4719b300bc90eec
Enrico Sada <enrico@sada.io>
2017-03-10 12:45:12 +0100

    initial commit

---
+++ c1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            l1.Class1.Ciao();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
